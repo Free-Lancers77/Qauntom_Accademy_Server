e8badd19e6f8ae4de1dea4b3f74f9905957c72ec
Hussien320 <hussienzoughaib@gmail.com>
2025-03-12 23:00:48 +0200

    git commit "a";

---
+++ serever.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { DbConnect } from './db/db.config.js';
+dotenv.config();
+const app = express();
+//middleware
+app.use(express.json());//for json data
+app.use(cookieParser());//for cookies
+app.use(cors({
+    credentials: true,}));//for cors  Prevents Unauthorized Access to Sensitive Data
+//
+const Port=process.env.PORT || 5000;
+app.get("/",(req,res)=>{
+    res.send("hi");
+})
+app.listen(Port, () => {
+    DbConnect();
+    console.log(`Server is running on port ${Port}`);
+});
